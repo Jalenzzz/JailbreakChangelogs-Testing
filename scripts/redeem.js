3a963610c8d5db2dc270c5d997938053616e0da2
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2025-04-03 06:35:59 +0300

    changed redeem endpoint

--- scripts/redeem.js
+++ scripts/redeem.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     try {
       const response = await fetch(
-        "https://api3.jailbreakchangelogs.xyz/codes/redeem",
+        "https://api.testing.jailbreakchangelogs.xyz/codes/redeem",
         {
           method: "POST",
           headers: {
