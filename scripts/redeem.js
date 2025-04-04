73199d50634445ef33e599adcf602dd6c4a11d19
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2025-04-04 23:27:23 +0300

    change redeem endpoint

--- scripts/redeem.js
+++ scripts/redeem.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            token: token,
+            owner: token,
             code: code,
           }),
         }
